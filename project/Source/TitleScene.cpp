139424aa1cac2184532e66016847468a176dae1e
CucumberRole <>
2025-11-04 14:10:51 +0900

    キー名変更

--- project/Source/TitleScene.cpp
+++ project/Source/TitleScene.cpp
@@ -25,5 +25,5 @@ void TitleScene::Draw()
 	DrawString(0, 20, Version(), GetColor(255,255,255));
 	DrawString(0, 0, "TITLE SCENE", GetColor(255,255,255));
 	DrawFormatString(100, 100, GetColor(255,255,255), "%4.1f", 1.0f / Time::DeltaTime());
-	DrawString(100, 400, "Push [P]Key To Play", GetColor(255, 255, 255));
+	DrawString(100, 400, "Push [SHIFT]Key To Play", GetColor(255, 255, 255));
 }
