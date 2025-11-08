ecba111a00f4fc42607386042de6ed364eab6ad4
aa <aa>
2025-11-08 13:27:51 +0900

    ちょっと変更

--- project/Source/PlayScene.cpp
+++ project/Source/PlayScene.cpp
@@ -24,6 +24,9 @@ void PlayScene::Update()
 
 void PlayScene::Draw()
 {
-	DrawString(0, 0, "PLAY SCENE", GetColor(255, 255, 255));
+	int h = GetFontSize();
+	DrawString(0, 0 + h * 0, "PLAY SCENE", GetColor(255, 255, 255));
+	DrawFormatString(0, 0 + h * 1, GetColor(255, 255, 255), "%4.1f", 1.0f / Time::DeltaTime());
+
 	DrawString(100, 400, "Push [F2]Key To Title", GetColor(255, 255, 255));
 }
--- project/Source/TitleScene.cpp
+++ project/Source/TitleScene.cpp
@@ -27,7 +27,7 @@ void TitleScene::Draw()
 	DrawString(0, 0 + h * 0, "TITLE SCENE", GetColor(255, 255, 255));
 	DrawString(0, 0 + h * 1, Version(), GetColor(255, 255, 255));
 	DrawFormatString(0, 0 + h * 2, GetColor(255, 255, 255), "%4.1f", 1.0f / Time::DeltaTime());
-	DrawString(0, 0 + h * 3, "FontSize:", GetColor(255, 255, 255));
+	DrawFormatString(0, 0 + h * 3, GetColor(255, 255, 255), "FontSize:%d", h);
 	DrawString(100, 400, "Push [SHIFT]Key To Play", GetColor(255, 255, 255));
 
 }
