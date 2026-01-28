e809e9141060520c35a56f51b6f6282964acd584
CucumberRole <jin.0105.otoko@gmail.com>
2026-01-28 17:49:42 +0900

    no message

--- project/Source/FakeFloor.cpp
+++ project/Source/FakeFloor.cpp
@@ -10,7 +10,7 @@ bool FakeFloor::CheckHitBall(float px, float py, float pw, float ph)
 
 FakeFloor::FakeFloor(int sx, int sy)
 {
-	hImage = LoadGraph("data/image/NewBlock.png");
+	hImage = DxLib::LoadGraph("data/image/NewBlock.png");
 	x = sx;
 	y = sy;
 }
@@ -26,5 +26,5 @@ void FakeFloor::Update()
 
 void FakeFloor::Draw()
 {
-	DrawRectGraph(static_cast<int>(x), static_cast<int>(y), 0, 0, 64, 64, hImage, TRUE);
+	DxLib::DrawRectGraph(static_cast<int>(x), static_cast<int>(y), 0, 0, 64, 64, hImage, TRUE);
 }
